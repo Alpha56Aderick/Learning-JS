ffc88d94d710c96543719fa4a03325cad5202779
ACIE <alpha562023@gmail.com>
2025-07-19 14:45:58 +0300

    BASICS- DOM

---
+++ index.js
@@ -0,0 +1,28 @@
+ //const title = document.getElementById("main-heading");
+//title.style.color = 'red'
+/*
+const listItems = document.querySelectorAll('.movie');
+ 
+ for (i = 0; i<listItems.length; i++) {
+     listItems[i].style.color = 'yellow';
+ }
+
+ // creating anew element 
+ const ul = document.querySelector('ul'); 
+ const li = document.createElement('li');   
+ 
+ ul.append(li);
+ li.innerText = 'The Lord of the Rings';
+ // adding and removing attribute
+ li.setAttribute('id','main-heading');
+ li.removeAttribute('id');
+
+ // changing the class of an element
+li.classList.add('movie');
+li.classList.remove('movie');
+// remove element
+li.remove();
+*/
+
+    // transversing the DOM
+    
